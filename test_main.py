8b5507ad55e4009a1155fb378d3b6317d0b65106
vsquared67 <34039167+vsquared67@users.noreply.github.com>
2025-04-30 11:43:08 -0700

    Update test_main.py
    Adding assert False to test the build

--- test_main.py
+++ test_main.py
@@ -24,6 +24,7 @@ def client():
 
 def test_health(client):
     response = client.get('/')
+    assert False  # This will cause the test to fail
     assert response.status_code == 200
     assert response.json == 'Healthy'
 
